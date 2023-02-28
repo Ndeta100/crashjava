11001fe48dd26e1c72d36c9bff451b37d6b37f30
ndeta <ndetainnocent3@gmail.com>
2023-02-28 18:02:46 +0200

    main class

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello data!");
+        System.out.println(10+10);
+        System.out.println("me and my bro");
+        int number1=10;
+        int number2=3;
+        System.out.println(number2+number1);
+        double pi=3.14;
+        boolean isAdult=true;
+        char a='e';
+        byte n=  126;
+        int amount=1_000_000_008;
+        System.out.println(amount);
+        //arithmetic
+        int numberOne=10;
+        int numberTwo=20;
+        System.out.println(numberOne+numberTwo);
+        System.out.println(numberOne-numberTwo);
+        System.out.println(numberOne*numberTwo);
+        System.out.println(numberOne/numberTwo);
+        System.out.println(numberOne%numberTwo);
+        //increment and decrement
+        int number=0;
+//        int increment=number+1;
+//        int decrement=number-1;
+//        System.out.println(increment);
+//        System.out.println(decrement);
+        System.out.println(number++);
+        System.out.println(number);
+        System.out.println(number--);
+
+    }
+}
